45c92cc439072a7c83938bb391f6e284fd1d0533
14D00944 <14D00944@skvdiweb.com>
2020-04-17 17:59:57 +0900

    add mockup daata

--- server/api/graphql/database.py
+++ server/api/graphql/database.py
@@ -3,7 +3,9 @@ import datetime
 
 from mongoengine import connect
 from server.api.graphql.models import (
-  RankModel, RankModeModel, CommonCodeModel
+  RankModel, RankModeModel, # Tseter
+  CommonCodeModel, UserModel, # Common
+  TodoCateModel, TodoItemModel # Todo
 )
 
 MONGO_DTATBASE="graphql-example"
@@ -15,23 +17,52 @@ print( conn.server_info() )
 
 # 기초 데이터 Insert 함수
 def init_db():
+  reg_user = "admin"
   reg_dttm = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+      
+  UserModel(email="admin@gmail.com", pwd="1234", name="admin", age=999, cell_phone="01012345678", reg_user=reg_user, reg_dttm=reg_dttm).save()
+  UserModel(email="admin2@gmail.com", pwd="1234", name="admin2", age=999, cell_phone="01012345678", reg_user=reg_user, reg_dttm=reg_dttm).save()
+  
+  CommonCodeModel(code_grp="TODO_STATUS", code_value="FINISH", code_name="완료", reg_user=reg_user, reg_dttm=reg_dttm).save()
+  CommonCodeModel(code_grp="TODO_STATUS", code_value="WORKING", code_name="진행중", reg_user=reg_user, reg_dttm=reg_dttm).save()
+  CommonCodeModel(code_grp="TODO_STATUS", code_value="READY", code_name="대기", reg_user=reg_user, reg_dttm=reg_dttm).save()
+  CommonCodeModel(code_grp="TODO_STATUS", code_value="GIVE_UP", code_name="포기", reg_user=reg_user, reg_dttm=reg_dttm).save()
+  CommonCodeModel(code_grp="TODO_STATUS", code_value="CANCEL", code_name="취소", reg_user=reg_user, reg_dttm=reg_dttm).save()
+  CommonCodeModel(code_grp="TODO_STATUS", code_value="DELETE", code_name="삭제", reg_user=reg_user, reg_dttm=reg_dttm).save()
+  
+  TodoCateModel(cate_grp="WEB_DEVELOPMENT", cate_value="JAVA", cate_name="Java", reg_user=reg_user, reg_dttm=reg_dttm).save()
+  TodoCateModel(cate_grp="WEB_DEVELOPMENT", cate_value="PYTHON", cate_name="Python", reg_user=reg_user, reg_dttm=reg_dttm).save()
+  TodoCateModel(cate_grp="WEB_DEVELOPMENT", cate_value="JAVASCRIPT", cate_name="Javascript", reg_user=reg_user, reg_dttm=reg_dttm).save()
+
+  TodoItemModel(
+    title="ToDo List 만들기", 
+    main_cate="WEB_DEVELOPMENT", 
+    sub_cate=None,
+    status="READY", 
+    desc="나의 첫번째 ToDo List 만들기.",
+    start_date=datetime.datetime.now().strftime("%Y%m%d"),
+    due_date="20201231",
+    working_day=None,
+    reg_user=reg_user,
+    reg_dttm=reg_dttm
+  ).save()
+  
 
   for idx in range(1, 5):
     mode = f"{ idx }x{ idx }"
 
-    RankModeModel(mode=mode).save()
+    RankModeModel(
+      mode=mode,
+      reg_user=reg_user,
+      reg_dttm=reg_dttm
+    ).save()
   
     for idx in range(50):
       RankModel(
-        name="heo", 
-        mode=mode, 
-        score=idx, 
+        name="heo",
+        mode=mode,
+        score=idx,
         is_mobile=False, 
+        reg_user=reg_user,
         reg_dttm=reg_dttm
       ).save()
-  
-  CommonCodeModel(code_grp="TODO_STATUS", code_value="1", code_name="대기", reg_dttm=reg_dttm).save()
-  CommonCodeModel(code_grp="TODO_STATUS", code_value="2", code_name="진행중", reg_dttm=reg_dttm).save()
-  CommonCodeModel(code_grp="TODO_STATUS", code_value="3", code_name="완료", reg_dttm=reg_dttm).save()
-  CommonCodeModel(code_grp="TODO_STATUS", code_value="999", code_name="오류", reg_dttm=reg_dttm).save()
--- server/api/graphql/models.py
+++ server/api/graphql/models.py
@@ -1,20 +1,95 @@
 # api/models.py
 from mongoengine import Document
 from mongoengine.fields import (
-  StringField, IntField, BooleanField
+  StringField, IntField, BooleanField, EmailField
 )
 
+class BaseDocument(Document):
+  meta = {
+    'allow_inheritance': True,
+    'abstract': True
+  }
 
-class RankModeModel(Document):
+  reg_user = StringField(required=True)
+  reg_dttm = StringField(required=True)
+  upd_user = StringField()
+  upd_dttm = StringField()
+  
+
+  
+class CommonCodeModel(BaseDocument):
+  meta = {
+    "collection": "common_code_mst"
+  }
+  code_grp = StringField(required=True, unique=True, unique_with='code_value')
+  code_value = StringField(required=True)
+  code_name = StringField(required=True)
+  desc = StringField()
+  
+  
+  
+class TodoCateModel(BaseDocument):
+  meta = {
+    'collection': 'todo_cate'
+  }
+  cate_grp = StringField(required=True, unique=True, unique_with='cate_value')
+  cate_value = StringField(required=True)
+  cate_name = StringField(required=True)
+  desc = StringField()
+  
+
+  
+class TodoItemModel(BaseDocument):
+  meta = {
+    'collection': 'todo_item'
+  }
+  title = StringField(required=True)
+  main_cate = StringField(required=True)
+  sub_cate = StringField()
+  
+  status = StringField()
+  desc = StringField()
+  
+  start_date = StringField()
+  due_date = StringField()
+  working_day = IntField()
+
+
+class UserModel(BaseDocument):
+  meta = {
+    'collection': 'user_info'
+  }
+  email = EmailField(required=True, unique=True)
+  pwd = StringField(required=True)
+  name = StringField(required=True)
+  age = IntField()
+  cell_phone = StringField()
+  
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+'''
+  Test Models
+'''
+class RankModeModel(BaseDocument):
   meta = {
     'collection': 'game_rank_modes'
   }
   mode = StringField(description='2048 grame mode.')
-  reg_dttm = StringField()
-  upd_dttm = StringField()
 
 
-class RankModel(Document):
+class RankModel(BaseDocument):
   meta = {
     'collection': 'game_ranking'
   }
@@ -22,18 +97,3 @@ class RankModel(Document):
   name = StringField()
   score = IntField()
   is_mobile = BooleanField()
-  reg_dttm = StringField()
-  upd_dttm = StringField()
-
-class CommonCodeModel(Document):
-  meta = {
-    'collection': 'common_code_mst'
-  }
-  code_grp = StringField(required=True)
-  code_value = StringField(required=True)
-  code_name = StringField(required=True)
-  desc = StringField()
-  reg_dttm = StringField()
-  upd_dttm = StringField()
-
-  
